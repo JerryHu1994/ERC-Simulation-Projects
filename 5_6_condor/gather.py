1516200f5748bb28b426d66f004dc01667a3a1b5
Jieru Hu <hjr01211@gmail.com>
2018-05-08 09:55:54 -0500

    fix gather

--- 5_6_condor/gather.py
+++ 5_6_condor/gather.py
@@ -17,12 +17,12 @@ Crank_Points=np.zeros((np.size(EvalIndices),1));
 
 for i in range(40):
 	start,end = i*57, i*57+56
-	file_name = 'Aro_2000_160C_IgDel_Comparison_Full_CA_%d'%i
+	file_name = 'npz/Aro_2000_160C_IgDel_Comparison_Full_CA_%d.npz'%i
 	curr_file = np.load(file_name)
 
 	# copy tau mix
-	curr_tau_mix[start:end,:] = curr_file['mix_Tau'][start:end,:]
+	Tau_mix[start:end,:] = curr_file['mix_Tau'][start:end,:]
 	# copy tau iso
-	curr_tau_iso_rk[start:end,:] = curr_file['pure_Tau'][start:end,:]
-
+	Tau_Iso_RK[start:end,:] = curr_file['pure_Tau'][start:end,:]
+save_name = "Aro_2000_160C_IgDel_Comparison_Full_CA.npz"
 np.savez(save_name,mix_Tau=Tau_mix, pure_Tau=Tau_Iso_RK,Crank_Angle=Crank_Points)
